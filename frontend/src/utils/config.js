127a0644d2ad148f7d2c9322199c46040969d1fe
Nandini20092003 <155342797+Nandini20092003@users.noreply.github.com>
2024-07-22 17:45:13 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,2 @@
-// export const BASE_URL = 'http://localhost:4000/api/v1'
+export const BASE_URL = 'http://localhost:4000/api/v1'
 
-// utils/config.js
-export const BASE_URL = 'https://divyaindia-backend.onrender.com/api/v1';
