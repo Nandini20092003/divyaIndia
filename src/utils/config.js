740cd092924ecc52336e6d91cede4f0d4ea5626c
Nandini20092003 <155342797+Nandini20092003@users.noreply.github.com>
2024-07-22 18:42:50 +0530

    C-2

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
-// utils/config.js
+// // utils/config.js
 
-export const BASE_URL = 'https://divyaindia-backend.onrender.com/api/v1';
+// export const BASE_URL = 'https://divyaindia-backend.onrender.com/api/v1';
 
 
-// export const BASE_URL = 'http://localhost:4000/api/v1'
+export const BASE_URL = 'http://localhost:4000/api/v1'
 
